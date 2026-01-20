3783d26936845f1a83455c75684dd74627ec4d97
akhil-singh28 <as1575828@gmail.com>
2026-01-20 12:02:17 +0530

    first commit

---
+++ lect_20_Jan.java
@@ -0,0 +1,35 @@
+// GIVEN AN ARRAY , COUNT NUMBER OF ELEMENTS HAVING ATLEAST ONE ELEMENT GREATER THAN ITSELF
+
+// PSUEDO CODE-
+// 1. iterate and find the max element from the array.
+// 2. Iterate and get the number of element that are not equal to max
+// 3. Increment the count
+import java.util.Scanner;
+public class lect_20_Jan {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        int arr[]=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<max){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
+// Observation: for every max element there won't be any element greater than itself.
+
+
---
+++ que.java
@@ -0,0 +1,9 @@
+// public class que {
+    
+// }
+
+
+// GIVEN AN ARRAY OF INTEGERS NUMS CALCULATE THE PIVOT INDEX OF THIS ARRAY. THE PIVOT INDEX IS THE INDEX WHERE THE SUM OF ALL THE NUMBERS
+// STRCITLY TO THE LEFT OF THE INDEX EQUALS TO THE SUM OF NUMBERS STRICTLY TO THE INDEX RIGHT
+// If the index on the left edge of the array then the left sum is zero beacuse there are no elemnets to the left. this also applies to the 
+// right edge of the array. writtn the left most pivot index. if no such index exist return -1.
