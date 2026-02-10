cde1191e65082acd6403deca0f8ae5aa8e16b405
AKHIL PRATAP SINGH <as1575828@gmail.com>
2026-02-10 11:51:37 +0530

    Initial commit

---
+++ everyRowSum.java
@@ -0,0 +1,26 @@
+// WAP TO FIND SUM OF EVERY ROW OF A MATRIX
+
+import java.util.Scanner;
+public class tenFeb{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter r:");
+        int r=sc.nextInt();
+        System.out.println("Enter c:");
+        int c=sc.nextInt();
+        int [][]arr=new int[r][c];
+        System.out.println("Enter ele:");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int sum=0;
+        for(int i=0;i<c;i++){
+            for(int j=0;j<r;j++){
+                sum=sum+arr[i][j];
+            }
+        }
+        System.out.println("Sum is:"+sum);
+    }
+}
