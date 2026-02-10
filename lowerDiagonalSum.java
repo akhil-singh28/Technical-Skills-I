9d67887603a911b242607bcc43cbe3a9d79b2c2f
AKHIL PRATAP SINGH <as1575828@gmail.com>
2026-02-10 11:49:24 +0530

    Initial commit

---
+++ lowerDiagonalSum.java
@@ -0,0 +1,28 @@
+// SUM OF LOWER DIAGONAL ELEMENT OF A MATRIX
+
+import java.util.Scanner;
+public class tenFeb{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter r:");
+        int r=sc.nextInt();
+        System.out.println("Enter c:");
+        int c=sc.nextInt();
+        int [][]arr=new int[r][c];
+        System.out.println("Enter ele:");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int sum=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(i>j){
+                    sum=sum+arr[i][j];
+                }
+            }
+        }
+        System.out.println("Sum is:"+sum);
+    }
+}
